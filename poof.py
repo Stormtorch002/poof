99270a8bd75849fb270b0982c5f2250927b67405
Mumatae <pianister123@gmail.com>
2020-08-11 14:28:26 -0400

    fix ?poof command

--- poof.py
+++ poof.py
@@ -1,10 +1,15 @@
-import discord
 from discord.ext import commands
-from .config import TOKEN
+from config import TOKEN
 
 
 bot = commands.Bot(command_prefix='?')
 bot.remove_command('help')
+bot.load_extension('jishaku')
+
+
+@bot.event
+async def on_ready():
+    print('Ready')
 
 
 @bot.command()
@@ -14,17 +19,23 @@ async def poof(ctx, amount: int = 5):
         await ctx.send('You cannot ?poof more than 99 messages at a time.')
         return
 
-    def predicate(message):
-        return message.channel.id == ctx.channel.id and message.author.id == ctx.author.id
+    messages = []
 
-    messages = await ctx.channel.history(limit=amount + 1).filter(predicate).flatten()
+    async for message in ctx.channel.history():
+
+        if message.author.id == ctx.author.id:
+            messages.append(message)
+
+            if len(messages) == amount + 1:
+                break
+
     await ctx.channel.delete_messages(messages)
     await ctx.send(f'{ctx.author.mention} used `?poof`, {amount} messages were deleted.', delete_after=5)
 
 
 @poof.error
 async def poof_error(ctx, error):
-    print(f'Something went wrong: {error}')
+    await ctx.send(f'Something went wrong: {error}')
 
     
 bot.run(TOKEN)
